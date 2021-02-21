46dc81faa35e2904057547da10c1b73eac0cf3f2
Marc LeBlanc <7050295+marcleblanc2@users.noreply.github.com>
2021-02-21 16:52:46 -0700

    Starting cloud-init user-data bootstrap script

--- user-data.sh
+++ user-data.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
-echo "${input}" > /tmp/input.txt
+# Configure script to log console output to a log file
+exec > >(tee -ia /var/log/valheim.log)
+exec 2>&1
 
+# Write start to log
+echo "$(date -Iseconds) - Starting $@"
+
+# Update packages
+echo "$(date -Iseconds) - Starting sudo yum update -y"
+sudo yum update -y
+
+# Ping to validate internet connectivity
+echo "$(date -Iseconds) - Pinging Google to validate internet connectivity"
+ping -c 3 -W 3 google.com
+
+
+# Install Valheim server
+
+
+
+# Copy Valheim server world files?
+
+
+
+# Reboot to finish installing updates
+echo "$(date -Iseconds) - Rebooting, if needed"
+sudo needs-restarting -r || sudo shutdown -r
 
