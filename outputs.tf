ba728886e1e81c3de7bf5cb3106621d8c1b69150
Marc LeBlanc <7050295+marcleblanc2@users.noreply.github.com>
2021-02-21 01:09:48 -0700

    Added outputs for public DNS and IP

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,10 @@
-# DNS address
+output "ec2-instance-public-dns-name" {
+
+    value = aws_instance.valheim-server-ec2-instance.public_dns
+  
+}
+output "ec2-instance-public-ip" {
+
+    value = aws_instance.valheim-server-ec2-instance.public_ip
+  
+}
